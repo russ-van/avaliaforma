4f201ee090349ec4618d3351e45dd38fcff963b5
Aluno <aluno@edu.senac.br>
2023-09-26 20:16:38 -0300

    Avaliação 26/09/2023

---
+++ js/texto_abertura.js
@@ -0,0 +1,24 @@
+function inicio() {
+    const textoOriginal = "Planeta Alien";
+    const elementoTexto = document.querySelector("p")
+    const audio = document.querySelector("audio")
+    const btn = document.querySelector("button")
+    const atrasoRapido = 5000 // Atraso em milissegundos (2 segundos)
+    const atrasoDevagar = 7000
+
+    setTimeout(function () {
+        btn.style.display = "none"
+        audio.play()
+    }, atrasoRapido);
+
+    setTimeout(function () {
+        elementoTexto.textContent = textoOriginal
+    }, atrasoDevagar);
+
+
+}
+
+function inicioAvaliacao(){
+    alert(`Olá avalia`)
+    console.log(`Olá avalia no navegador`)
+}
